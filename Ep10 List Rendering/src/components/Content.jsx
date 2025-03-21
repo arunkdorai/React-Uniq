e13188afec97a35bde246a42691de75f68a07913
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-21 22:17:05 +0530

    Ep10 List Redering updated

--- Ep10 List Rendering/src/components/Content.jsx
+++ Ep10 List Rendering/src/components/Content.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Shop from "./Shop";
-  
-  const Content = () => {
-  return (
-    <main>
-      <Shop />
-    </main>
-  );
+import { FaTrashCan } from "react-icons/fa6";
+
+
+const Content = () => {
+  let [items, setItems] = useState([
+    { id: 1, label: "Html & CSS", checked: true },
+    { id: 2, label: "JavaScript", checked: true },
+    { id: 3, label: "React JS", checked: false },
+  ]);
+
+  return <main>
+    {/* <Shop /> */}
+    <div>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+    <ul>
+      {
+        items.map((item)=> {
+          return (
+            <li key={item.id} className="item">
+              <input type="checkbox" checked = {item.checked} />
+              <label >{item.label}</label>
+              <FaTrashCan role="button" tabIndex={0} />
+            </li>
+          )
+        })
+      }
+    </ul>
+    </main>;
 };
 
 export default Content;
