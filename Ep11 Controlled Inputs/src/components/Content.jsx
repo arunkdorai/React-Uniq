b1f844240b48d3bb34d00619f2c0c596cfa13fb9
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-21 23:17:53 +0530

    Ep11 Controlled Inputs updated

--- Ep11 Controlled Inputs/src/components/Content.jsx
+++ Ep11 Controlled Inputs/src/components/Content.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Shop from "./Shop";
 import { FaTrashCan } from "react-icons/fa6";
-
+import { FaEdit } from "react-icons/fa";
 
 const Content = () => {
   let [items, setItems] = useState([
@@ -10,26 +10,53 @@ const Content = () => {
     { id: 3, label: "React JS", checked: false },
   ]);
 
-  return <main>
-    {/* <Shop /> */}
-    <div>
-      <input type="text" />
-      <button>Add</button>
-    </div>
-    <ul>
-      {
-        items.map((item)=> {
+  let [newItem, setNewItem] = useState("");
+  let [isEditing, setIsEditing] = useState(false);
+
+  let handleChecked = (id) => {
+    let newListItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+
+    setItems(newListItems);
+  };
+
+  let handleUpdate = () => {
+    setIsEditing(true)
+  }
+
+  return (
+    <main>
+      {/* <Shop /> */}
+      <div>
+        <input
+          type="text"
+          value={newItem}
+          placeholder="Add New Item"
+          onChange={(e) => {
+            setNewItem(e.target.value);
+          }}
+        />
+        <button>{isEditing ? "Save" : "Add"}</button>
+      </div>
+      <ul>
+        {items.map((item) => {
           return (
             <li key={item.id} className="item">
-              <input type="checkbox" checked = {item.checked} />
-              <label >{item.label}</label>
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleChecked(item.id)}
+              />
+              <label>{item.label}</label>
+              <FaEdit role="button" tabIndex={0} onClick={handleUpdate} />
               <FaTrashCan role="button" tabIndex={0} />
             </li>
-          )
-        })
-      }
-    </ul>
-    </main>;
+          );
+        })}
+      </ul>
+    </main>
+  );
 };
 
 export default Content;
