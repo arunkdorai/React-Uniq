7de6732344a9cb0359c26e281b72392d4aa6bd5a
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-26 01:11:48 +0530

    Ep15 Fake Server updated

--- Ep15 Fake Server/src/App.jsx
+++ Ep15 Fake Server/src/App.jsx
@@ -6,30 +6,25 @@ import Products from "./components/Products";
 import SignUp from "./components/SignUp";
 import TodoApp from "./components/TodoApp";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  let user = "Arun"
+  let user = "Arun";
   return (
     <div className="app">
       <Router>
-      <ol>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to={`/login/${user}`}>Login</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/signup">SignUp</Link></li>
-        <li><Link to="/todoapp">TodoApp</Link></li>
-      </ol>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login/:newUser" element={<Login />} />
-          <Route path="/products" element={<Products />} >
+          <Route path="/products" element={<Products />}>
             <Route index element={<ProductList />} />
             <Route path="list" element={<ProductList />} />
             <Route path="details" element={<ProductDetails />} />
           </Route>
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/todoapp" element={<TodoApp />} />
+          <Route path="/todo" element={<TodoApp />} />
         </Routes>
       </Router>
     </div>
