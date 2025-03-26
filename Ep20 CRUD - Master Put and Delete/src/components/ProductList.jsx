2ae5efe3cecc70139e162b9590493647c81232ca
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-27 01:01:12 +0530

    Ep20 CRUD - added UpdateProduct using PUT

---
+++ Ep20 CRUD - Master Put and Delete/src/components/ProductList.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Atom } from "react-loading-indicators";
+import useFetch from "./custom-hook/useFetch";
+import { MdAddShoppingCart } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { MdOutlineFolderDelete } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+
+const ProductList = () => {
+
+  let navigate = useNavigate()
+
+  let { products, error, isLoading } = useFetch(
+    "http://localhost:5000/products"
+  );
+
+  if (isLoading) {
+    return (
+      <div>
+        <center>
+          <Atom
+            color="#f5145a"
+            size="large"
+            text="Loading"
+            textColor="#f87c00"
+          />
+        </center>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Product List</h1>
+        {products.length !== 0 && (
+          <section className="products">
+            {products.map((product) => (
+              <Card
+                key={product.id}
+                style={{ width: "18rem" }}
+                className="product"
+              >
+                <center>
+                  <Card.Img
+                    variant="top"
+                    src={product.image}
+                    style={{ width: "9rem", height: "12rem" }}
+                  />
+                </center>
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>${product.price}</Card.Text>
+                </Card.Body>
+                <Card.Footer
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button variant="primary">
+                    <MdAddShoppingCart />
+                  </Button>
+                  <Button variant="secondary" onClick={()=>{
+                    navigate(`/update/${product.id}`)
+                  }}>
+                    <FaEdit />
+                  </Button>
+                  <Button variant="danger">
+                    <MdOutlineFolderDelete />
+                  </Button>
+                </Card.Footer>
+              </Card>
+            ))}
+          </section>
+        )}
+        {error && <p>{error}</p>}
+      </div>
+    );
+  }
+};
+
+export default ProductList;
