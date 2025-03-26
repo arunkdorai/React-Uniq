b64d3d6b005e8dca4baa603c214bc97a931a8c4f
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-26 20:31:30 +0530

    CRUD with Fetch added

---
+++ Ep19 CRUD with Fetch/src/App.jsx
@@ -0,0 +1,38 @@
+import Home from "./components/Home";
+import Login from "./components/Login";
+import ProductDetails from "./components/ProductDetails";
+import ProductList from "./components/ProductList";
+import Products from "./components/Products";
+import SignUp from "./components/SignUp";
+import TodoApp from "./components/TodoApp";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound";
+import NewProduct from "./components/NewProduct";
+
+function App() {
+  // let user = "Arun";
+  return (
+    <div className="app">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/:newUser" element={<Login />} />
+          <Route path="/products" element={<Products />}>
+            <Route index element={<ProductList />} />
+            <Route path="list" element={<ProductList />} />
+            <Route path="details" element={<ProductDetails />} />
+          </Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/todo" element={<TodoApp />} />
+          <Route path="/newProduct" element={<NewProduct />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
