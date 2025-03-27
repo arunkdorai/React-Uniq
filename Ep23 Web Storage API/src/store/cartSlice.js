df75a03b86f6ad3e31fa7d8e0a3bd053e9b41182
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-27 19:15:08 +0530

    Ep 23 Web Storate API - Master Local Storage with Redux Toolkit

--- Ep23 Web Storage API/src/store/cartSlice.js
+++ Ep23 Web Storage API/src/store/cartSlice.js
@@ -1,16 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let dataFromWeb = JSON.parse(localStorage.getItem("cart"))
+
 const cartSlice = createSlice({
   name: "cart1",
-  initialState: [],
+  initialState: dataFromWeb,
   reducers: {
     addItem(state, action) {
       state.push(action.payload);
+
+      localStorage.setItem("cart", JSON.stringify([...state]))
     },
     removeItem(state, action) {
         let itemId = action.payload //1
         // [1,2,3,4 ] => 2!== 1
         let newProducts = state.filter(cartProduct => cartProduct.id !== itemId)
+
+        localStorage.setItem("cart", JSON.stringify([...newProducts]))
 
         return newProducts
     },
